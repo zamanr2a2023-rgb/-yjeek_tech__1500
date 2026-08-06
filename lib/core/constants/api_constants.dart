24ea8cce7b4822178cd7d5d2f38e296c04833552
Palash Chandra Roy <palashtpi21@gmail.com>
2026-08-06 17:14:45 +0600

    new version update

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -3,5 +3,5 @@
 /// Physical phone over USB: run `adb reverse tcp:3000 tcp:3000` so the
 /// device's `127.0.0.1:3000` tunnels to this PC's Yjeek backend.
 abstract final class ApiConstants {
-  static const String baseUrl = 'http://103.208.183.248:3000/api/v1';
+  static const String baseUrl = 'https://api.yjeektech.com/api/v1';
 }
